fd647f80b47d26a45c7eec9597af7e5c6fbe24c1
NanYangEvelyn <ny2187@nyu.edu>
2023-09-13 22:03:21 -0400

    finish

--- sketch.js
+++ sketch.js
@@ -1,8 +1,29 @@
+let width = 1000;
+let height = 1000;
+let triangleShortWidth = 450;
+let triangleLongWidth = 700;
+
 function setup() {
-    createCanvas(windowWidth,windowHeight);
+    createCanvas(width,height);
 
 }
 
 function draw() {
-    background(123,232,98);
+    background(0);
+
+
+    translate(0,300);
+    stroke(0);
+    fill(250,235,215);
+    triangle(0,0,0,700,triangleShortWidth,0);
+    triangle(297,240,0,700,1000,700);
+
+    translate(1000,400);
+    rotate(radians(180));
+    stroke(0);
+    fill(250,235,215);
+    triangle(0,0,0,700,triangleShortWidth,0);
+    triangle(297,240,0,700,1000,700);
+
+
 }
